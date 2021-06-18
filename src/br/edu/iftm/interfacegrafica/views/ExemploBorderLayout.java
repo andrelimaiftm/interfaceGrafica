b6b1161dbc5a217844c5f15db1a94404ae79a0dc
Andre Chaves Lima <andrelima@iftm.edu.br>
2021-06-17 22:55:12 -0300

    Atualização do código

--- src/br/edu/iftm/interfacegrafica/views/ExemploBorderLayout.java
+++ src/br/edu/iftm/interfacegrafica/views/ExemploBorderLayout.java
@@ -6,26 +6,30 @@ import javax.swing.WindowConstants;
 import java.awt.BorderLayout;
 
 public class ExemploBorderLayout {
+    
     public static void main(String[] args) {
+
         JFrame janela = new JFrame("Exemplo BorderLayout");
         janela.setBounds(50, 50, 500, 500);
         janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //Cria o layout da tela
         //BorderLayout layout = new BorderLayout();
         //janela.setLayout(layout);
+
         JButton botao1 = new JButton("Botão 1 (PAGINA TOPO)");
         JButton botao2 = new JButton("Botão 2 (CENTRO)");
         JButton botao3 = new JButton("Botão 3 (INICIO LINHA)");
         JButton botao4 = new JButton("Botão 4 (FIM LINHA)");
         JButton botao5 = new JButton("Botão 5 (PAGINA FIM)");
-                
-
 
         janela.add(botao1, BorderLayout.PAGE_START);
         janela.add(botao2, BorderLayout.CENTER);
         janela.add(botao3, BorderLayout.LINE_START);
         janela.add(botao4, BorderLayout.LINE_END);
         janela.add(botao5, BorderLayout.PAGE_END);
-        
+
         janela.setVisible(true);
+        
     }
 }
--- src/br/edu/iftm/interfacegrafica/views/PrimeiraJanela.java
+++ src/br/edu/iftm/interfacegrafica/views/PrimeiraJanela.java
@@ -11,62 +11,56 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class PrimeiraJanela {
-    
+
     public static void main(String[] args) {
-        //Cria uma janela
+        
+
+        //Criar uma janela
         JFrame janela = new JFrame("Título da minha primeira Janela");
-        
-        //Define a possição inicial e o tamanho da janela
         janela.setBounds(100, 100, 500, 500);
-
-        //Fefine o comportamento de encerrar o programa ao clicar em X
-        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //Define o layout para nulo (deverá informar as coordenadas dos compontes)
         janela.setLayout(null);
-        
-        //Cria um campo de texto (paenas uma linha)
-        JTextField campoUsuario = new JTextField();
 
-        //Define a posição e o tamanho do campo de texto
-        campoUsuario.setBounds(50, 100, 150, 30);// Y inicial = 100; Y final = 130
-
-        //Adiciona JTextField no painel (JPainel)
-        janela.add(campoUsuario);
-
-        //Cria uma label
+        //cria label -> que apresenta informações para usuário
         JLabel labelUsuario = new JLabel("Usuário: ");
-        labelUsuario.setBounds(50,80,50,20);
-
-
         JLabel labelSenha = new JLabel("Senha: ");
-        labelSenha.setBounds(50, 161, 50, 20);
-        JPasswordField campoSenha = new JPasswordField();
-        campoSenha.setBounds(50, 181, 150, 30);
 
+        //cria um cmapo de texto (apenas uma linha)
+        JTextField campoUsuario = new JTextField();
+        JPasswordField camposSenha = new JPasswordField();
+
+        //criar um botão
         JButton botao = new JButton("Entrar");
-        botao.setBounds(70, 250, 100, 30);
+
+        //adicionando a posição e o tamanho do campo na tela
+        labelUsuario.setBounds(50, 80, 50, 20);
+        campoUsuario.setBounds(50, 100, 150, 30);
+        labelSenha.setBounds(50, 160, 50, 20);
+        camposSenha.setBounds(50, 181, 150, 30);
+        botao.setBounds(50, 250, 100, 30);
+
+        //Adicionar o Jlabel e o JTextFild na janela
+        janela.add(labelUsuario);
+        janela.add(campoUsuario);
+        janela.add(labelSenha);
+        janela.add(camposSenha);
+        janela.add(botao);
 
         botao.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) {                
                 String usuario = campoUsuario.getText();
-                String senha = campoSenha.getText();
-                System.out.printf("Usuario: %s \tSenha: %s \n", usuario, senha);
+                String senha = camposSenha.getText();
+                System.out.printf("Usuário: %s \tSenha: %s\n",usuario,senha);
                 //System.out.println("Clickou no botão");
-                
             }
         });
 
-        janela.add(labelUsuario);
-        janela.add(campoSenha);
-        janela.add(labelSenha);
-        janela.add(botao);
-       
+
+        //Define  comportamento ao encerrar programa ao clicar em X
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         //Define a janela como visível
         janela.setVisible(true);
-
-        
-        
     }
+    
 }
